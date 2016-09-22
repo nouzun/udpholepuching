61bac2f8bcd4cbd05058c02302e2f2355295d45a
Necip Onur Uzun <nouzun@users.noreply.github.com>
2016-09-22 11:43:41 +0300

    Update ClientSender.java

--- UDPHolePunchingClient/src/ClientSender.java
+++ UDPHolePunchingClient/src/ClientSender.java
@@ -25,29 +25,27 @@ public class ClientSender implements Runnable {
 		inFromUser = new BufferedReader(new InputStreamReader(System.in));
 	}
 	
-    public void run() {
+	public void run() {
         do{
-  	      try {
-  	    	  sentence = inFromUser.readLine();
-	  	      sendData = sentence.getBytes();
-	  	      sendPacket = new DatagramPacket(sendData, sendData.length, address, port);
-	  	      clientSocket.send(sendPacket);
-	  	      System.out.println("Data sent to " + address.getHostAddress() + ":" + port);
-	  	      
-	  	      if(!address.getHostAddress().equals(InetAddress.getLocalHost().getHostAddress())) // Send a local packet if peers behind the same NAT
-	  	      {
-		  	      sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getLocalHost(), localPort);
-		  	      clientSocket.send(sendPacket);
-		  	      System.out.println("Data sent to " + InetAddress.getLocalHost().getHostAddress() + ":" + localPort);
-	  	      }
-	  	      
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+	  	      	sentence = inFromUser.readLine();
+		  	sendData = sentence.getBytes();
+		  	sendPacket = new DatagramPacket(sendData, sendData.length, address, port);
+		  	clientSocket.send(sendPacket);
+		  	System.out.println("Data sent to " + address.getHostAddress() + ":" + port);
+		  	      
+		  	if(!address.getHostAddress().equals(InetAddress.getLocalHost().getHostAddress())) // Send a local packet if peers behind the same NAT
+		  	{
+			    sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getLocalHost(), localPort);
+			    clientSocket.send(sendPacket);
+			    System.out.println("Data sent to " + InetAddress.getLocalHost().getHostAddress() + ":" + localPort);
+		  	}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
   	      
         } while(!sentence.equals("quit"));
-        
-        clientSocket.close();
-    }
+        	clientSocket.close();
+	}
 }
