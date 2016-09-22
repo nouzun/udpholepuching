7712d7ba3104d718ae9f1493cb5c25ca50a6746f
Necip Onur Uzun <nouzun@users.noreply.github.com>
2016-09-22 11:38:00 +0300

    Update Server.java

--- UDPHolePunchingServer/src/Server.java
+++ UDPHolePunchingServer/src/Server.java
@@ -9,34 +9,34 @@ import java.util.Map;
 public class Server {
 	public static void main(String args[]) throws Exception {
 		boolean newClient;
-    	DatagramSocket socket = new DatagramSocket(5666);
-    	ArrayList<ClientInfo> clients = new ArrayList<ClientInfo>();
-        System.out.println("Server is started!");
-        
-        while (true) {
-        	newClient = true;
-            byte[] receiveData = new byte[1024];
-
-        	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(receivePacket);
-            
-            System.out.println("Server received a packet!");
-            
-  	      	for (int i=0; i < clients.size(); i++) {
-  	      		ClientInfo client = clients.get(i);
-  	      		if(client.address.equals(receivePacket.getAddress()) && client.port == receivePacket.getPort())
-  	      		{
-  	      			newClient = false;
-  	      			break;
-  	      		}
-  	      	}
-
-  	      	if(newClient)
-  	      		clients.add(new ClientInfo(receivePacket.getAddress(), receivePacket.getPort()));
-            
+	    	DatagramSocket socket = new DatagramSocket(5666);
+	    	ArrayList<ClientInfo> clients = new ArrayList<ClientInfo>();
+	        System.out.println("Server is started!");
+	        
+	        while (true) {
+	            newClient = true;
+	            byte[] receiveData = new byte[1024];
+	
+	            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	            socket.receive(receivePacket);
+	            
+	            System.out.println("Server received a packet!");
+	            
+	  	    for (int i=0; i < clients.size(); i++) {
+	  	        ClientInfo client = clients.get(i);
+	  	      	if(client.address.equals(receivePacket.getAddress()) && client.port == receivePacket.getPort())
+	  	      	{
+	  	      	    newClient = false;
+	  	      	    break;
+	  	        }
+	  	    }
+	
+	  	    if(newClient)
+	  	        clients.add(new ClientInfo(receivePacket.getAddress(), receivePacket.getPort()));
+	            
 		    Thread thread = new Thread(new ServerResponder(socket, receivePacket, clients));
 		    thread.start(); 
-        }
+	        }
 	}
 }
 
