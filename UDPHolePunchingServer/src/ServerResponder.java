7ac94664e6306b6473d1c8d626d3b640fac9fd32
Necip Onur Uzun <nouzun@users.noreply.github.com>
2016-09-22 11:40:35 +0300

    Update ServerResponder.java

--- UDPHolePunchingServer/src/ServerResponder.java
+++ UDPHolePunchingServer/src/ServerResponder.java
@@ -13,7 +13,7 @@ import java.util.Set;
 
 public class ServerResponder implements Runnable {
 
-	DatagramSocket socket = null;
+    DatagramSocket socket = null;
     DatagramPacket packet = null;
     byte[] sendData = new byte[1024];
     ArrayList<ClientInfo> clients = null;
@@ -29,57 +29,51 @@ public class ServerResponder implements Runnable {
     	
         String sentence = new String(packet.getData(), 0 , packet.getLength());
         System.out.println("RECEIVED: " + sentence);
-    	
+
         //String capitalizedSentence = sentence.toUpperCase();
         //sendData = capitalizedSentence.getBytes();
-        
-        	
+
     	ByteArrayOutputStream bos = null;
     	ObjectOutput out = null;
     	try {
     		if(sentence.startsWith("<Hi!>"))
     		{
-    	        String capitalizedSentence = sentence.toUpperCase();
-    	        sendData = capitalizedSentence.getBytes();
+    	            String capitalizedSentence = sentence.toUpperCase();
+    	            sendData = capitalizedSentence.getBytes();
     	        
-                response = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
+                    response = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
 
-                socket.send(response);
+                    socket.send(response);
     		}
     		else
     		{
-      	      	for (int i=0; i < clients.size(); i++) {
+      	      	    for (int i=0; i < clients.size(); i++) {
       	      		ClientInfo client = clients.get(i);
       	      		if(client.address.equals(packet.getAddress()) && client.port == packet.getPort())
       	      		{
       	      			clients.remove(i);
       	      			break;
       	      		}
-      	      	}
+      	      	    }
     			
-	    		bos = new ByteArrayOutputStream();
-	        	out = new ObjectOutputStream(bos);   
-				out.writeObject(clients);
+	    	    bos = new ByteArrayOutputStream();
+	            out = new ObjectOutputStream(bos);   
+		    out.writeObject(clients);
 				
-	        	sendData = bos.toByteArray();
+	            sendData = bos.toByteArray();
 	        	
 	            response = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
 
 	            socket.send(response);
 	        	
-	        	out.close();
-	        	bos.close();
+	            out.close();
+	            bos.close();
     		}
-    		
-
-            
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        	
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
         System.out.println(clients.size());
-
     }
 }
